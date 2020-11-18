c2f105f11d491b956261d633e657d52bc1b58f68
Elandro <elandro.caniela@gmail.com>
2020-11-18 19:44:02 -0300

    adding category.model.ts

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,17 +1,18 @@
 import {InMemoryDbService} from "angular-in-memory-web-api";
-
+import {Category} from "./pages/categories/shared/category.model";
 
 export class InMemoryDataBase implements InMemoryDataBase
 {
     cretateDb()
     {
-        const categories = 
+        const categories: Category[] = 
         [
-            {id:1, name: "Moradia", description: "Pagamento de Contas da Casa"},
-            {id:2, name: "Saúde", description: "Plano de Saúde e Remédios"},
-            {id:3, name: "Lazer", description: "Cinema, parques, praia etc"},
-            {id:4, name: "Salário", description: "Recebimento de Sálario"},
-            {id:5, name: "Freelas", description: "Trabalhos como Freelancer"}
+            {id:1, nome: "Moradia", description: "Pagamento de Contas da Casa"},
+            {id:2, nome: "Saúde", description: "Plano de Saúde e Remédios"},
+            {id:3, nome: "Lazer", description: "Cinema, parques, praia etc"},
+            {id:4, nome: "Salário", description: "Recebimento de Sálario"},
+            {id:5, nome: "Freelas", description: "Trabalhos como Freelancer"}
+            
         ];
 
         return { categories };
